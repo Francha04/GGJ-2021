61ea6b9ff50dd79c968eb2764def75127e1f6e4c
Ezequiel <eze_ruiz_pacios@hotmail.com>
2021-01-28 00:18:47 -0300

    Level manager script

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private LevelManager _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void LoadLevel(string name)
+    {
+        //Verificar que el nombre de la escena concuerde con el nombre del parametro
+        //y que la escena este en el build
+        SceneManager.LoadScene(name);
+    }
+}
