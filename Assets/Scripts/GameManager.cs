5f3b8371ac8b2a461a4539728f4424523d011397
Ezequiel <eze_ruiz_pacios@hotmail.com>
2021-01-28 17:42:40 -0300

    Agregado ciertos parametros para las condiciones de Lost y Win

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameManager _instance;
+    public int amountOfErrors;
+    private int errorsTolerance = 10;
+    public bool playerLostTheGame;
+
+    private void Awake()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        playerLostTheGame = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool CheckIfPlayerLostTheGame()
     {
-        
+        return amountOfErrors >= errorsTolerance;
+    }
+
+    public void ResetStats()
+    {
+        amountOfErrors = 0;
+        playerLostTheGame = false;
+    }
+
+    private void Update()
+    {
+        playerLostTheGame = CheckIfPlayerLostTheGame();
+
+        if(playerLostTheGame)
+        {
+            //Cargar escena final ?
+            Debug.Log("Perdiste");
+        }
     }
 }
