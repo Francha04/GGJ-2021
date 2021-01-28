20011633f628ef6b91c81eed75de1833e6706f23
Ezequiel <eze_ruiz_pacios@hotmail.com>
2021-01-28 17:11:19 -0300

    Drag and drop mechanic completed

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] Canvas canvas;
+    private CanvasGroup canvasGroup;
+    private RectTransform rectTransform;
+    [SerializeField] ItemOrganizer itemOrganizer;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        this.transform.localScale = new Vector3(1f, 1f, 0f);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        Debug.Log(eventData.position);
+        Debug.Log(itemOrganizer.transform.position);
+
+        if(!itemOrganizer.mouseInside)
+        {
+            itemOrganizer.OnExitItem(this);
+        }
+    }
+}
---
+++ Assets/Scripts/ItemOrganizer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemOrganizer : MonoBehaviour, IDropHandler, IPointerEnterHandler,IPointerExitHandler
+{
+    public List<GameObject> listOfItemSlots;
+    public bool mouseInside;
+    
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            foreach (var itemslot in listOfItemSlots)
+            {
+                if (!itemslot.GetComponent<ItemSlot>().isOccupied || itemslot.GetComponent<ItemSlot>().item == eventData.pointerDrag)
+                {
+                    //Establezco que va a estar ocupado por un item
+                    itemslot.GetComponent<ItemSlot>().isOccupied = true;
+                    //Muevo el elemento a la posicion fija del itemslot.transform
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = itemslot.GetComponent<RectTransform>().anchoredPosition;
+                    eventData.pointerDrag.GetComponent<Transform>().localScale = new Vector3(0.75f, 0.75f);
+                    //Obtener el item
+                    itemslot.GetComponent<ItemSlot>().item = eventData.pointerDrag;
+                    break;
+                }
+            }
+        }
+    }
+
+    //Por aca iria lo contrario del OnDrop, establecer en falso el itemSlot
+    public void OnExitItem(DragAndDrop dragAndDrop)
+    {
+        foreach (var itemSlot in listOfItemSlots)
+        {           
+            if (itemSlot.GetComponent<ItemSlot>().item == dragAndDrop.gameObject)
+            {
+                itemSlot.GetComponent<ItemSlot>().isOccupied = false;
+                itemSlot.GetComponent<ItemSlot>().item = null;
+                break;
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseInside = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseInside = false;
+    }
+}
